6847ebb28cb1cae1da6b22d1ab68586d9858b5a9
Tanay <102220360+tanayios123@users.noreply.github.com>
2023-11-23 14:56:59 +0530

    commit

---
+++ Fructus iOS 16/Fructus/App/FructusApp.swift
@@ -0,0 +1,18 @@
+
+
+import SwiftUI 
+
+@main
+struct FructusApp: App {
+  @AppStorage("isOnboarding") var isOnboarding: Bool = true
+  
+  var body: some Scene {
+    WindowGroup {
+      if isOnboarding {
+        OnboardingView()
+      } else {
+        ContentView()
+      }
+    }
+  }
+}
---
+++ Fructus iOS 16/Fructus/Model/FruitModel.swift
@@ -0,0 +1,15 @@
+
+
+import SwiftUI
+
+// MARK: - FRUITS DATA MODEL
+
+struct Fruit: Identifiable {
+  var id = UUID()
+  var title: String
+  var headline: String
+  var image: String
+  var gradientColors: [Color]
+  var description: String
+  var nutrition: [String] 
+}
